02cc69135cd549c716f30a8084ec9c1642bc6f9c
DomeldManangan <domeldmanangan@gmail.com>
2026-05-13 21:47:22 +0800

    feat: implement base templates and login interface with integrated design system

--- RealEstateDemo/settings.py
+++ RealEstateDemo/settings.py
@@ -138,7 +138,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    BASE_DIR / "RealEstateDemo" / "static",
+]
 
 # Media files (uploaded images)
 MEDIA_URL = "/media/"
